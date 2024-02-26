06172bae757a002b1fa8584b09420f50d2172709
Ikechukwu Fortune <99960870+FIKE110@users.noreply.github.com>
2024-02-26 09:03:12 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const config={
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DB
+    database:process.env.MYSQL_DB,
+    port: 28942
 }
 
 let pool=null
